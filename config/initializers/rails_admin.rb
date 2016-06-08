eb8f04dcd33abbca101f2b76352cd3900bad6ef5
shabalinsky <shabalinsky@gmail.com>
2016-06-08 12:55:00 +0600

    ckeditor description

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -31,4 +31,11 @@ RailsAdmin.config do |config|
     # history_index
     # history_show
   end
+
+  config.model Project do
+    edit do
+      include_all_fields
+      field :description, :ck_editor
+    end
+  end
 end
