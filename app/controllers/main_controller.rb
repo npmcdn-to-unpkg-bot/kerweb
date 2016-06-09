83dacc59e4a2f44824af68d453fca4b498561dfd
shabalinsky <shabalinsky@gmail.com>
2016-06-09 18:56:39 +0600

    new randomizer

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,7 @@ class MainController < ApplicationController
     @projects          = Project.all.order_by(order: 'asc')
     @employers         = Employee.all.order_by(order_number: 'asc')
     @technologies      = Technology.all
-    @statistics = get_random_statistics(3)
+    @statistics = get_random_statistics_new
   end
 
   def get_random_statistics(n)
@@ -13,6 +13,20 @@ class MainController < ApplicationController
       offset = rand(cnt-n)
     end
     Statistic.skip(offset).limit(n)
+  end
+
+  def get_random_statistics_new
+    rand1 = rand(Statistic.count)
+    rand2 = rand(Statistic.count)
+    while rand2==rand1
+      rand2 = rand(Statistic.count)
+    end
+    rand3 = rand(Statistic.count)
+    while rand3==rand1 or rand3==rand2
+      rand3 = rand(Statistic.count)
+    end
+
+    [Statistic.offset(rand1).first, Statistic.offset(rand2).first, Statistic.offset(rand3).first]
   end
 
 end
